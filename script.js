9f7fb288b1128438e7ba72f64420cc61b2a46432
Moruan06 <moruan.mattar@gmail.com>
2025-08-08 15:25:29 -0300

    Implentação da logica para mostrar os livros e uso do formulario

--- script.js
+++ script.js
@@ -1,12 +1,42 @@
 const myLibrary = [];
-const container = document.querySelector("#container");
+const container = document.querySelector("main");
+const formBox = document.querySelector("#formBox");
+const bookForm = document.querySelector("#newBookForm");
+
+document.getElementById("addBookBtn").addEventListener('click', (e) =>{
+  e.preventDefault();
+  formBox.classList.remove("hidden");
+  console.log("Button clicked");
+})
+
+document.getElementById("cancelBtn").addEventListener('click', (event) => {
+  event.preventDefault();
+  formBox.classList.add("hidden");
+});
+
+bookForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const titleInputForm = document.querySelector("#title").value;
+  const authorInputForm = document.querySelector("#author").value;
+  const imageInputForm = document.querySelector("#image").value;
+  const readInputForm = document.querySelector("#read").checked;
+  addBookToLibrary(
+    titleInputForm,
+    authorInputForm,
+    imageInputForm,
+    readInputForm
+  );
+  bookForm.reset();
+  displayBook(myLibrary);
+  formBox.classList.add("hidden");
+});
 
 function Book(title, author, bookCover, read) {
-  (this.id = crypto.randomUUID()),
-    (this.title = title),
-    (this.author = author),
-    (this.bookCover = bookCover),
-    (this.read = read);
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.bookCover = bookCover;
+  this.read = read;
 }
 
 function addBookToLibrary(titleInput, authorInput, bookCoverInput, readInput) {
@@ -14,10 +44,11 @@ function addBookToLibrary(titleInput, authorInput, bookCoverInput, readInput) {
   myLibrary.push(newBook);
 }
 
-function addBookCard(arr) {
+function displayBook(arr) {
   const cardsHTML = arr
-    .map((item) =>
-        `<div class="card">
+    .map(
+      (item) =>
+        `<div class="card" data-id="${item.id}">
             <img src="${item.bookCover}" alt="Capa do livro">
             <p class="title"> ${item.title} </p>
             <p class="author"> by ${item.author} </p>
@@ -25,7 +56,5 @@ function addBookCard(arr) {
     )
     .join("");
 
-  container.innerHTML += cardsHTML;
+  container.innerHTML = cardsHTML;
 }
-
-addBookCard(myLibrary);
