5954a45a154ad8aae1b24f2493b4ef4768ed66d7
Moruan06 <moruan.mattar@gmail.com>
2025-08-09 16:50:02 -0300

    Função de deletar e alternar o status do livro adicionada; novos botões no card

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-const myLibrary = [];
+let myLibrary = [];
 const container = document.querySelector("main");
 const formBox = document.querySelector("#formBox");
 const bookForm = document.querySelector("#newBookForm");
 
-document.getElementById("addBookBtn").addEventListener('click', (e) =>{
+container.addEventListener('click', (event) =>{
+  if(event.target.matches(".readBtn")){
+    console.log("butao clicado")
+    const card = event.target.closest(".card");
+    const bookId = card.dataset.id;
+
+    const bookToUpdate = myLibrary.find(book => book.id === bookId)
+
+    bookToUpdate.toogleStatus();
+    displayBook(myLibrary)
+  }
+
+  if(event.target.matches(".deleteBtn")){
+    const card = event.target.closest(".card");
+    const bookId = card.dataset.id;
+
+    myLibrary = myLibrary.filter(book => book.id !== bookId);
+    displayBook(myLibrary);
+  }
+})
+
+document.getElementById("addBookBtn").addEventListener("click", (e) => {
   e.preventDefault();
   formBox.classList.remove("hidden");
   console.log("Button clicked");
-})
+});
 
-document.getElementById("cancelBtn").addEventListener('click', (event) => {
+document.getElementById("cancelBtn").addEventListener("click", (event) => {
   event.preventDefault();
+  bookForm.reset();
   formBox.classList.add("hidden");
 });
 
@@ -37,6 +59,7 @@ function Book(title, author, bookCover, read) {
   this.author = author;
   this.bookCover = bookCover;
   this.read = read;
+  this.toogleStatus = () => this.read = !this.read;
 }
 
 function addBookToLibrary(titleInput, authorInput, bookCoverInput, readInput) {
@@ -52,9 +75,17 @@ function displayBook(arr) {
             <img src="${item.bookCover}" alt="Capa do livro">
             <p class="title"> ${item.title} </p>
             <p class="author"> by ${item.author} </p>
+            <div class="cardButtons">
+              <button class="readBtn">${
+                item.read
+                  ? "<i class='bi bi-eye' style='color: green' title='read'></i>"
+                  : "<i class='bi bi-eye-slash' style='color: darkgoldenrod' title='not read yet'></i>"
+              }</button>
+              <button class="deleteBtn" title='delete'><i class="bi bi-trash3"></i></button>
+            </div>
          </div>`
     )
     .join("");
-
+  container.innerHTML = "";
   container.innerHTML = cardsHTML;
 }
